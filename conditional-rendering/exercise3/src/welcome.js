5ec90b86f83aa9af1d710c6fb03c966259742efc
Tuna <tunakrdnz@gmail.com>
2023-02-04 11:15:41 +0300

    did the exercise3

---
+++ conditional-rendering/exercise3/src/welcome.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Age} from './age';
+
+export class Welcome extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome, {this.props.name}!</p>
+                {this.props.age>18 && this.props.age<65
+                ? <Age age={this.props.age}/>
+                : " "}
+            </div>
+        );
+    }
+}
