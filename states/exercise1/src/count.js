56232ffff959c7480753aea7503b9b03cf9d84a8
Tuna <tunakrdnz@gmail.com>
2023-02-04 18:18:24 +0300

    did the exercise1

---
+++ states/exercise1/src/count.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export class Counter extends React.Component {
+    state = {
+        count: 0
+    }
+    constructor(props) {
+        super(props)
+        setInterval(() => {
+            this.setState((state) => {
+                return {
+                    count: state.count + 1,
+                }
+            })
+        }, 1000)
+    }
+    render() {
+        return(
+            <div>
+                <h1>Count: {this.state.count}</h1>
+            </div>
+        )
+    }
+}
