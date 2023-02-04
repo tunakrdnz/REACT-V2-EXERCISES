2e1a4328aadecb2a825b32a1025cf8257498aec1
Tuna <tunakrdnz@gmail.com>
2023-02-04 11:30:50 +0300

    did the exercise4

---
+++ conditional-rendering/exercise4/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Welcome } from './welcome';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Welcome name="Tuna" age={35}/>);
+
+
