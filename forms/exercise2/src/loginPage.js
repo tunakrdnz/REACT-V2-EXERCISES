b17b71faadd33a07eb99ab9227fba587870a2439
Tuna <tunakrdnz@gmail.com>
2023-02-10 12:07:37 +0300

    did the exercise2

---
+++ forms/exercise2/src/loginPage.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+
+export class LoginPage extends React.Component {
+    state = {
+        username: '',
+        password: '',
+        remember: false
+    }
+
+    handleInputChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        const type = event.target.type;
+        const checked = event.target.checked;
+        
+        this.setState({
+            [name]: type === 'checkbox' ? checked :value,
+        })
+    }
+
+    render() {
+        return (
+              <div>
+                 <input 
+                    name = "username" 
+                    value = {this.state.username} 
+                    onChange = {this.handleInputChange}
+                 />
+                 <input 
+                    name = "password" 
+                    type = "password" 
+                    value = {this.state.password}
+                    onChange = {this.handleInputChange}
+                 />
+                 <input 
+                    name = "remember" 
+                    type = "checkbox" 
+                    checked = {this.state.remember}
+                    onChange = {this.handleInputChange}
+                 />
+              </div>
+        )
+    }
+
+
+}
