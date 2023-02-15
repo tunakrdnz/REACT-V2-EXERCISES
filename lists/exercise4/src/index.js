44dbe7923429e33d9bb60e479725f61cbb780e29
Tuna <tunakrdnz@gmail.com>
2023-02-15 23:40:38 +0300

    did the exercise4

---
+++ lists/exercise4/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {ToDoList} from './todolist';
+
+const toDoList=[
+  {id:1, title:"Do some exercise"},
+  {id:2, title:"Clean the house"},
+  {id:3, title:"Take a walk"},
+  {id:4, title:"Do some meditation"}
+];
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ToDoList todos={toDoList}/>);
+
