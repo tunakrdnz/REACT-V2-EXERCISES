97e734f4ee2a41102ee4ea694fde3cdad341795e
Tuna <tunakrdnz@gmail.com>
2023-02-16 17:21:48 +0300

    did the exercise1

---
+++ styling-components/eexercise1/src/welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export class Welcome extends React.Component {
+    render() {
+        return(
+            <div className='container'>
+                <h1>Welcome!</h1>
+            </div>
+        )
+    }
+}
