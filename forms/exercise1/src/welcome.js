9253c015109dd1e22baa9e2115d0bcb27a41a024
Tuna <tunakrdnz@gmail.com>
2023-03-03 11:46:06 +0300

    fixed punctuation

--- forms/exercise1/src/welcome.js
+++ forms/exercise1/src/welcome.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export class Welcome extends React.Component {
     render() {
-        return <p>Welcome, {this.props.name}</p>;
+        return <p>Welcome, {this.props.name}!</p>
     }
 }
